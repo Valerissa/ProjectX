d1c97b0b587f4b682bde616586da515f6a6ee1ea
Valerissa <valeria.marasan@gmail.com>
2021-09-21 23:43:24 -0400

    sharing my knowledge with you

---
+++ src/test/java/Hello.java
@@ -0,0 +1,21 @@
+public class Hello {
+    public static void main(String[] args) {
+
+
+        System.out.println("Creating test project for you");
+
+
+    int num = 10;
+
+    //confirm please
+
+
+
+
+
+
+
+
+
+    }
+}
